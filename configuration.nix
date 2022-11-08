9fbe68ad9564791a55002b307cab65b8f8115d73
Anton Arapov <anton@deadbeef.mx>
2022-11-08 17:56:08 +0100

    use zsh/starship
    Signed-off-by: Anton Arapov <anton@deadbeef.mx>

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,25 @@
       };
     };
 
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+        add_newline = false;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+      };
+    };
+    programs.zsh = {
+      enable = true;
+      initExtra = ''
+        eval "$(starship init zsh)"
+      '';
+    };
+
   };
 
 }
