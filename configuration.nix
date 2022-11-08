5a00e3c344b852b0eee4c033cf915b1ea0342c82
Anton Arapov <anton@heimdall.lan>
2022-11-08 17:20:07 +0100

    add home-manager
    Signed-off-by: Anton Arapov <anton@heimdall.lan>

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,45 @@
 { pkgs, ... }:
-
 {
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
   programs.zsh.enable = true;
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.anton = { pkgs, ... }: {
+    home.stateVersion = "22.05";
+
+    programs.git = {
+      enable = true;
+      userName = "Anton Arapov";
+      userEmail = "anton@deadbeef.mx";
+      signing = {
+        key = "134C02E813889057DA2F3FDBEDDD4C5DAA149BBE";
+        signByDefault = true;
+      };
+
+      extraConfig = {
+        init.defaultBranch = "master";
+        core = {
+          editor = "vim";
+          whitespace = "trailing-space,space-before-tab";
+        };
+        url = {
+          "git@github.com:" = {
+            insteadOf = "https://github.com/";
+          };
+        };
+        url = { # makes rust's cargo happy again
+          "https://github.com/rust-lang/crates.io-index" = {
+            insteadOf = "https://github.com/rust-lang/crates.io-index";
+          };
+        };
+        pull = {
+          ff = "only";
+        };
+      };
+    };
+
+  };
+
 }
--- flake.nix
+++ flake.nix
@@ -5,12 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, darwin, nixpkgs }: {
+  outputs = { self, darwin, home-manager, nixpkgs }: {
     darwinConfigurations."heimdall" = darwin.lib.darwinSystem {
       system = "x86_64-darwin";
-      modules = [ ./configuration.nix ];
+      modules = [
+        home-manager.darwinModules.home-manager
+        ./configuration.nix
+      ];
     };
   };
 
